a642d760219ff693a0ffd617391d60ec39fd1c88
prathamesh-ai <prathamesh84.m@gmail.com>
2024-10-14 10:50:02 +0530

    Update t2m.py

--- t2m.py
+++ t2m.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 from langchain_groq import ChatGroq
 from langchain.chains import LLMMathChain, LLMChain
 from langchain.prompts import PromptTemplate
@@ -7,15 +7,21 @@ from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
 from langchain.callbacks import StreamlitCallbackHandler
 
-st.set_page_config(page_title="Text To Math Problem Solver And Data Serach Assistant")
+## Set upi the Stramlit app
+st.set_page_config(page_title="Text To MAth Problem Solver And Data Serach Assistant")
 st.title("Text To Math Problem Solver Uing Google Gemma 2")
 
 groq_api_key=st.sidebar.text_input(label="Groq API Key",type="password")
+
+
 if not groq_api_key:
     st.info("Please add your Groq APPI key to continue")
     st.stop()
 
-llm=ChatGroq(model="Gemma-7b-It",groq_api_key=groq_api_key)
+llm=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+
+## Initializing the tools
 wikipedia_wrapper=WikipediaAPIWrapper()
 wikipedia_tool=Tool(
     name="Wikipedia",
@@ -23,6 +29,8 @@ wikipedia_tool=Tool(
     description="A tool for searching the Internet to find the vatious information on the topics mentioned"
 
 )
+
+## Initializa the MAth tool
 
 math_chain=LLMMathChain.from_llm(llm=llm)
 calculator=Tool(
@@ -32,7 +40,7 @@ calculator=Tool(
 )
 
 prompt="""
-You are an agent tasked for solving user's mathemtical question. Logically arrive at the solution and provide a detailed explanation
+Your a agent tasked for solving users mathemtical question. Logically arrive at the solution and provide a detailed explanation
 and display it point wise for the question below
 Question:{question}
 Answer:
@@ -43,35 +51,39 @@ prompt_template=PromptTemplate(
     template=prompt
 )
 
-chain = LLMChain(llm=llm,prompt=prompt_template)
-reasoning_tool = Tool(
+## Combine all the tools into chain
+chain=LLMChain(llm=llm,prompt=prompt_template)
+
+reasoning_tool=Tool(
     name="Reasoning tool",
     func=chain.run,
     description="A tool for answering logic-based and reasoning questions."
 )
 
-assistant_agent = initialize_agent(
-    tools = [wikipedia_tool,calculator,reasoning_tool],
+## initialize the agents
+
+assistant_agent=initialize_agent(
+    tools=[wikipedia_tool,calculator,reasoning_tool],
     llm=llm,
-    agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=False,
-    handle_parsing_error = True
+    handle_parsing_errors=True
 )
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role":"assistant", "content":"Hi I am a Math Chatbot who can answer your maths queries."}
+    st.session_state["messages"]=[
+        {"role":"assistant","content":"Hi, I'm a Math chatbot who can answer all your maths questions"}
     ]
-    for msg in st.session_state.messages:
-        st.chat_message(msg['role']).write(msg['content'])
 
-    def generate_response(question):
-        response = assistant_agent.invoke({'input':question})
-        return response
-question = st.text_area("Enter your question: ", "I have 5 bananas. I gave 1 banana to my brother, 2 bananas to my Father, and I ate 1 banana. How many bananas am I left with?")
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+## LEts start the interaction
+question=st.text_area("Enter youe question:","I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruit do I have at the end?")
+
 if st.button("Get Answer"):
     if question:
-        with st.spinner("Generate response..."):
+        with st.spinner("Generate response.."):
             st.session_state.messages.append({"role":"user","content":question})
             st.chat_message("user").write(question)
 
